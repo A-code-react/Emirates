327345bb0bfd6bd86c6b1a209613910712eb2bbb
I1151 <123389489+I1151@users.noreply.github.com>
2023-03-07 19:33:31 +0530

    Add files via upload

---
+++ src/Components/Layout/Footer.jsx
@@ -0,0 +1,249 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer(props) {
+    return (
+        <div className="main-footer">
+            <div className="container">
+
+                {/* <div className="col-md-3 col-sm-6">
+                    <nav class="footer-nav">
+                        <span class="span-1A">
+                            <a href="Emirates" className='font'>Emirates</a>
+                        </span>
+                        (=)
+                        <span class="span-2A">
+                            <a href="Book a flight">Book a flight</a>
+                        </span>
+                    </nav>
+                </div>
+
+                <br /> */}
+
+                <div className="row">
+
+                    <div className="col-md-3 col-sm-6">
+                        <div className='font'>About Us</div>
+                        <div className='footer-data'>
+                            <ul className="list-unstyled">
+                                <li className='line-height'><a href="About Us">About Us</a></li>
+                                <li className='line-height'><a href="Carrers">Carrers</a></li>
+                                <li className='line-height'><a href="Media center">Media center</a></li>
+                                <li className='line-height'><a href="Our planet">Our planet</a></li>
+                                <li className='line-height'><a href="Our people">Our people</a></li>
+                                <li className='line-height'><a href="Our communities">Our communities</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-md-3 col-sm-6">
+                        <div className='font'>Help</div>
+                        <div className='footer-data'>
+                            <ul className="list-unstyled">
+                                <li className='line-height'><a href="Help and contact">Help and contact</a></li>
+                                <li className='line-height'><a href="Travel updates">Travel updates</a></li>
+                                <li className='line-height'><a href="Special assistance">Special assistance</a></li>
+                                <li className='line-height'><a href="Frequently asked questions">Frequently asked questions</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-md-3 col-sm-6">
+                        <div className='font'>Book</div>
+                        <div className='footer-data'>
+                            <ul className="list-unstyled">
+                                <li className='line-height'><a href="Book flights">Book flights</a></li>
+                                <li className='line-height'><a href="Travel services">Travel services</a></li>
+                                <li className='line-height'><a href="Transportation">Transportation</a></li>
+                                <li className='line-height'><a href="Search for flights">Search for flights</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-md-3 col-sm-6">
+                        <div className='font'>Manage</div>
+                        <div className='footer-data'>
+                            <ul className="list-unstyled">
+                                <li className='line-height'><a href="Check-in">Check-in</a></li>
+                                <li className='line-height'><a href="Chauffer-drive">Chauffer-drive</a></li>
+                                <li className='line-height'><a href="Flight status">Flight status</a></li>
+                                <li className='line-height'><a href="Manage your booking">Manage your booking</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div className="row">
+
+                    <div className="col-md-3 col-sm-6">
+                        <div className='font'>Before you fly</div>
+                        <div className='footer-data'>
+                            <ul className="list-unstyled">
+                                <li className='line-height'><a href="About Us">Baggage</a></li>
+                                <li className='line-height'><a href="Carrers">Visa and passport information</a></li>
+                                <li className='line-height'><a href="Media center">Health</a></li>
+                                <li className='line-height'><a href="Our planet">Travel information</a></li>
+                                <li className='line-height'><a href="Our people">Dubai International</a></li>
+                                <li className='line-height'><a href="Our communities">To and from the airport</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-md-3 col-sm-6">
+                        <div className='font'>Where we fly</div>
+                        <div className='footer-data'>
+                            <ul className="list-unstyled">
+                                <li className='line-height'><a href="Route map">Route map</a></li>
+                                <li className='line-height'><a href="Africa">Africa</a></li>
+                                <li className='line-height'><a href="Asia and Pacific">Asia and Pacific</a></li>
+                                <li className='line-height'><a href="Europe">Europe</a></li>
+                                <li className='line-height'><a href="The Americans">The Americans</a></li>
+                                <li className='line-height'><a href="The Middle East">The Middle East</a></li>
+                                <li className='line-height'><a href="Flights to all countries/territories">Flights to all countries/territories</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-md-3 col-sm-6">
+                        <div className='font'>Experience</div>
+                        <div className='footer-data'>
+                            <ul className="list-unstyled">
+                                <li className='line-height'><a href="Cabin features">Cabin features</a></li>
+                                <li className='line-height'><a href="Shop Emirates">Shop Emirates</a></li>
+                                <li className='line-height'><a href="What's on your flight">What's on your flight</a></li>
+                                <li className='line-height'><a href="Inflight entertainment">Inflight entertainment</a></li>
+                                <li className='line-height'><a href="Dining">Dining</a></li>
+                                <li className='line-height'><a href="Our lounges">Our lounges</a></li>
+                                <li className='line-height'><a href="Dubai Stopover">Dubai Stopover</a></li>
+                                <li className='line-height'><a href="Open Skies">Open Skies</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-md-3 col-sm-6">
+                        <div className='font'>Loyalty</div>
+                        <div className='footer-data'>
+                            <ul className="list-unstyled">
+                                <li className='line-height'><a href="Log in Emirates Skywards">Log in Emirates Skywards</a></li>
+                                <li className='line-height'><a href="Join Emirates Skywords">Join Emirates Skywords</a></li>
+                                <li className='line-height'><a href="Business Rewards benefits">Business Rewards benefits</a></li>
+                                <li className='line-height'><a href="Register your company">Register your company</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+                <hr />
+
+                <div className="row">
+
+                    <div className="col-md-4  col-sm-6">
+                        <div className='font'>Subscribe to our special offers</div>
+                        <div className='footer-data'>
+                            <ul className="list-unstyled">
+                                <li>
+                                    <h6 className="para">Save with our latest fares and offers.</h6>
+                                </li>
+                                <li>
+                                    <a href="Unsubscribe">Unsubscribe or change your preferences</a>
+                                </li>
+                                <li>
+                                    <span>
+                                        <input type="text" placeholder='Email Adress' size="13" className='email' />
+                                    </span>
+                                    <span>
+                                        <button className='btn'><small>Subscribe</small></button>
+
+                                    </span>
+                                </li>
+                                <li>
+                                    <p className="para">
+                                        For details on how we use your info, please see our
+                                        <br />
+                                        <a href="privacy policy">privacy policy</a>
+                                    </p>
+                                    <a href=""><img src="https://www.emirates.com/english/images/Apex-passenger-entertainment-award-2020_tcm233-7124905.svg" alt="F1" className='img' /></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4 col-sm-6">
+                        <div className='font'>Mobile Apps and Site</div>
+                        <div className='footer-data'>
+                            <ul className="list-unstyled">
+                                <li>
+                                    <p className="para">Book flights, check in, choose seats and meals</p>
+                                </li>
+                                <li>
+                                    <span><a href=""><img src="https://www.emirates.com/english/images/Apple_EN_tcm233-4143604.svg" alt="PS" /></a></span>
+                                    <span><a href=""><img src="https://www.emirates.com/english/images/Google_EN_tcm233-4143606.svg" alt="PS" /></a></span>
+                                </li>
+                                <br />
+                                <br />
+                                <br />
+                                <li>
+                                    <a href=""><img src="https://www.emirates.com/english/images/FGFA-STAR_tcm233-7444311.svg" alt="F2" className='img' /></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4 col-sm-6">
+                        <div className='font'>Connect with us</div>
+                        <div className='footer-data'>
+                            <ul className="list-unstyled">
+                                <li>
+                                    <p className="para">Share your Emirates experience</p>
+                                </li>
+                                <li>
+                                    <a href=""><img src="https://cdn.ek.aero/System/shared/Images/globalnavigation/facebook.svg" alt="I1" className='icon' /></a>
+                                    <a href=""><img src="https://cdn.ek.aero/System/shared/Images/globalnavigation/twitter.svg" alt="I2" className='icon' /></a>
+                                    <a href=""><img src="https://cdn.ek.aero/System/shared/Images/globalnavigation/youtube.svg" alt="I3" className='icon' /></a>
+                                    <a href=""><img src="https://cdn.ek.aero/System/shared/Images/globalnavigation/linkedin.svg" alt="I4" className='icon' /></a>
+                                    <a href=""><img src="https://cdn.ek.aero/System/shared/Images/globalnavigation/instagram.svg" alt="I5" className='icon' /></a>
+                                </li>
+                                <br />
+                                <br />
+                                <br />
+                                <li>
+                                    <a href=""><img src="https://www.emirates.com/english/images/skytrax_badge_2019_tcm233-4231454.svg" alt="F3" className='img' /></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+                <hr />
+
+                <div className="row">
+
+                    <div className="col-md-12 col-xs-3">
+                        <div className='footer-bottom-row'>
+                            <a href="Accessibility">Accessibility</a>
+                            <a href="Contact us">Contact us</a>
+                            <a href="Privacy policy">Privacy policy</a>
+                            <a href="Terms and conditions">Terms and conditions</a>
+                            <a href="Cookie Policy">Cookie Policy</a>
+                            <a href="Cybersecurity">Cybersecurity</a>
+                            <a href="Modern Slavery Act transparency statement">Modern Slavery Act transparency statement</a>
+                            <a href="Sitemap">Sitemap</a>
+                        </div>
+
+                    </div>
+                    <div className='copyright'>
+                        <p className='para-rights'>© 2023 The Emirates Group. All Rights Reserved.</p>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
